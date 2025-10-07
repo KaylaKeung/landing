13e6793b33278ab67c197dfc0cca74be726c3629
KaylaKeung <66004330+KaylaKeung@users.noreply.github.com>
2025-10-06 21:15:38 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
 
-// prints "hi" in the browser's dev tools cons
-const results = Splitting({ by: "chars" });
 
-results[0].el.style.setProperty("--noOfItems", results[0].chars.length);
+
+
